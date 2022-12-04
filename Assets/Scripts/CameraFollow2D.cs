433c38c740e4a82e5be4f1ab3e82a3fb32568b36
BuildTools <unconfigured@null.spigotmc.org>
2022-12-05 01:02:28 +0700

    sec

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2D : MonoBehaviour
+{
+
+	public float damping = 1.5f;
+	public Vector2 offset = new Vector2(0f, 0f);
+	private Transform player;
+	private int lastX;
+
+	void Start()
+	{
+		offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+		FindPlayer();
+	}
+
+	public void FindPlayer()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		lastX = Mathf.RoundToInt(player.position.x);
+	}
+
+	void Update()
+	{
+		if (player)
+		{
+			int currentX = Mathf.RoundToInt(player.position.x);
+			lastX = Mathf.RoundToInt(player.position.x);
+
+			Vector3 target;
+			target = new Vector3(player.position.x, player.position.y, transform.position.z);
+			Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
+			transform.position = currentPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 10.0f;
+    public GameObject killAura;
+    private Vector2 moveVector;
+    private bool attacking;
+    private float corutineTime = 0;
+    public float attackTime = 10;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        killAura.SetActive(false);
+        attacking = false;
+    }
+
+    void Update()
+    {
+        if (corutineTime > 0) corutineTime -= Time.deltaTime;
+        if (attacking && corutineTime <= 0) Attack(false);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Attack(true);
+        }
+        if (!attacking)
+        {
+            moveVector.x = Input.GetAxis("Horizontal");
+            moveVector.y = Input.GetAxis("Vertical");
+            rb.MovePosition(rb.position + moveVector.normalized * speed * Time.deltaTime);
+
+        }
+    }
+
+    void Attack(bool attack)
+    {
+        attacking = attack;
+        if (attack)
+        {
+            corutineTime = attackTime;
+            killAura.SetActive(true);
+        }
+        else
+        {
+            killAura.SetActive(false);
+            corutineTime = 0;
+        }
+    }
+}
